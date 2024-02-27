8b12f948c91bf2d9010d3bf02fde86ac1834f3ef
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-02-27 12:32:05 +0100

    added real time audio

--- app.js
+++ app.js
@@ -14,21 +14,21 @@ import googleRouter from "./routes/googleRoute.js";
 import { authenticated } from "./middlewares/authMiddleware.js";
 
 const appConfig = (app) => {
-   // app.use(function (req, res, next) {
-   //    res.header("Access-Control-Allow-Origin", "*");
-   //    res.header("Access-Control-Allow-Credentials", "true");
-   //    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-   //    res.header("Access-Control-Expose-Headers", "Content-Length");
-   //    res.header(
-   //      "Access-Control-Allow-Headers",
-   //      "Accept, Authorization, Content-Type, X-Requested-With, Range"
-   //    );
-   //    if (req.method === "OPTIONS") {
-   //      return res.sendStatus(200);
-   //    } else {
-   //      return next();
-   //    }
-   //  });
+   app.use(function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Credentials", "true");
+      res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+      res.header("Access-Control-Expose-Headers", "Content-Length");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Accept, Authorization, Content-Type, X-Requested-With, Range"
+      );
+      if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+      } else {
+        return next();
+      }
+    });
    app.use(cors(
       {
        origin: ["http://localhost:5173","https://veenotes.netlify.app"],
